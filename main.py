de9612767619b38ada55885916fda8a765fd89ba
IamBrod <pl.bererd@gmail.com>
2026-04-16 14:24:49 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import interfacegraphique
-import customtkinter as ctk
-
-
-ctk.set_appearance_mode("light")   
-ctk.set_default_color_theme("blue")
-root = ctk.CTk()
-interfacegraphique.PortraitRobotApp(root)
-root.mainloop()
