31aa009289c971f85e7deee2db519eba79bc7ffd
IamBrod <pl.bererd@gmail.com>
2026-04-22 19:35:24 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -55,7 +55,7 @@ else
     echo "--- PREMIER LANCEMENT ---"
     echo "Création de l'environnement '$ENV_NAME'..."
     echo "Cela peut prendre quelques minutes, veuillez patienter."
-    mamda env create -f environment.yml
+    mamda env create -f environment_portable.yml
     
     if [ $? -ne 0 ]; then
         echo "Erreur lors de la création de l'environnement."
